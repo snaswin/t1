87b03022f464a52c8bab62f3d037539e01d47f43
Aswin Raghav. Nirmaleswaran <anirmaleswaran@nanotronics.co>
2018-05-11 20:49:54 -0400

    full folder

---
+++ light_weight.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from skimage.exposure import adjust_sigmoid as asig
+from skimage.filters import threshold_otsu, threshold_local
+from skimage.util import invert
+
+
+i = cv2.imread("layer.jpg",0)
+l = cv2.imread("stage.jpg",0)
+l = l*10
+
+dev = 51
+L = cv2.GaussianBlur(l,(dev,dev),0)
+Linv = invert(L)
+Linv = (Linv-Linv.min())/Linv.max()
+
+
+#plt.imshow(Linv*i,cmap='gray')
+
+fig = plt.figure()
+sp = fig.add_subplot(131)
+sp2 = fig.add_subplot(132)
+sp3 = fig.add_subplot(133)
+
+sp.imshow(i,cmap='gray')
+sp2.imshow(Linv*i,cmap='gray')
+sp3.imshow(Linv*i+i,cmap='gray')
+
+plt.show()
